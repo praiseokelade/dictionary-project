6c36fa1662f0313606524c761066319241d1f743
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-06-07 23:09:01 +0100

    Tried to add axios and build search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("")
+  let [keyword, setKeyword] = useState("");
 
-    function searchKeyword(event) {
-        event.preventDefault();
-        alert(`Searching for the definition of ${keyword}`)
-    }
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
-    function updateKeyword(event) {
-        setKeyword(event.target.value)
-    }
-    
-    return (
-        <div className="Dictionary">
-            <form onSubmit={searchKeyword}>
-                <input type="search" onChange={updateKeyword}/>
-                <input type="submit" />
-            </form>
-        </div>
-    )
+  function searchKeyword(event) {
+    event.preventDefault();
+    alert(`Searching for the definition of ${keyword}`);
+
+    let apiKey = `017e2af483beec2b2e23fafdtf1f298o`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={searchKeyword}>
+        <input type="search" onChange={updateKeyword} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
