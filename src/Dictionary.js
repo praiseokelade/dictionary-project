81a5a6fc2c72b994a05b1525d9ec04ff4c9d7861
praiseokelade <151168193+praiseokelade@users.noreply.github.com>
2024-06-18 18:55:10 +0100

    Added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,29 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("dictionary");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handleImageResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function searchKeyword() {
     let apiKey = `017e2af483beec2b2e23fafdtf1f298o`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imagesApiUrl).then(handleImageResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +56,7 @@ export default function Dictionary() {
         </section>
 
         <Results information={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
